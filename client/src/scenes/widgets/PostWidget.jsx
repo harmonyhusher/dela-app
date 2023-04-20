20474a0d6a94fdf0a141567e0c4dfea63167b231
Diyor Safarov <tankisttanjig@gmail.com>
2023-04-20 15:06:05 +0300

    Update PostWidget.jsx

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -66,7 +66,7 @@ const PostWidget = ({
           height="auto"
           alt="post"
           style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-          src={`http://localhost:3001/assets/${picturePath}`}
+          src={`${URL}/${picturePath}`}
         />
       )}
       <FlexBetween mt="0.25rem">
