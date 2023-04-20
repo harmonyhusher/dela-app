65f96fff3a5e8fe581567257f690eaadd11cf28d
Diyor Safarov <tankisttanjig@gmail.com>
2023-04-21 01:45:44 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://affairssocial.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
