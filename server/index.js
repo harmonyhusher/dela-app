8c868d248f9327e1db015cb86fc7bdabe70c8c49
Diyor Safarov <tankisttanjig@gmail.com>
2023-04-21 01:22:20 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -31,10 +31,10 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://affairssoical.onrender.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
-
 /* FILE STORAGE */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
