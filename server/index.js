4f5939bda181e3dcbcdc218dbb3ad6e42d773526
Diyor Safarov <tankisttanjig@gmail.com>
2023-04-21 01:50:29 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -33,7 +33,6 @@ app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 /* FILE STORAGE */
