877765ed95850828aec612fd2a5a0fdd83079d48
Diyor Safarov <tankisttanjig@gmail.com>
2023-04-30 16:45:20 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -28,7 +28,8 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: '*'}));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
